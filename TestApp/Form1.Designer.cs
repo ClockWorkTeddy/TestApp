1bf463d4fd3db4ff21b0ff145588abbc9b11f939
Ivan <Vanka03@ya.ru>
2022-01-15 17:49:52 +0300

    Add project files.

---
+++ TestApp/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace TestApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gbGroups = new System.Windows.Forms.GroupBox();
+            this.dgGroups = new System.Windows.Forms.DataGridView();
+            this.gbUsers = new System.Windows.Forms.GroupBox();
+            this.dgUsers = new System.Windows.Forms.DataGridView();
+            this.gbAdd = new System.Windows.Forms.GroupBox();
+            this.bttAdd = new System.Windows.Forms.Button();
+            this.cbGroups = new System.Windows.Forms.ComboBox();
+            this.tbFio = new System.Windows.Forms.TextBox();
+            this.lblGroups = new System.Windows.Forms.Label();
+            this.lblFio = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.gbGroups.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgGroups)).BeginInit();
+            this.gbUsers.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgUsers)).BeginInit();
+            this.gbAdd.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbGroups
+            // 
+            this.gbGroups.Controls.Add(this.dgGroups);
+            this.gbGroups.Location = new System.Drawing.Point(12, 12);
+            this.gbGroups.Name = "gbGroups";
+            this.gbGroups.Size = new System.Drawing.Size(731, 189);
+            this.gbGroups.TabIndex = 0;
+            this.gbGroups.TabStop = false;
+            this.gbGroups.Text = "Группы";
+            // 
+            // dgGroups
+            // 
+            this.dgGroups.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgGroups.Location = new System.Drawing.Point(6, 19);
+            this.dgGroups.Name = "dgGroups";
+            this.dgGroups.Size = new System.Drawing.Size(719, 164);
+            this.dgGroups.TabIndex = 0;
+            // 
+            // gbUsers
+            // 
+            this.gbUsers.Controls.Add(this.dgUsers);
+            this.gbUsers.Location = new System.Drawing.Point(12, 207);
+            this.gbUsers.Name = "gbUsers";
+            this.gbUsers.Size = new System.Drawing.Size(731, 173);
+            this.gbUsers.TabIndex = 1;
+            this.gbUsers.TabStop = false;
+            this.gbUsers.Text = "Сотрудники";
+            // 
+            // dgUsers
+            // 
+            this.dgUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgUsers.Location = new System.Drawing.Point(6, 19);
+            this.dgUsers.Name = "dgUsers";
+            this.dgUsers.Size = new System.Drawing.Size(719, 148);
+            this.dgUsers.TabIndex = 1;
+            this.dgUsers.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dgUsers_MouseDown);
+            // 
+            // gbAdd
+            // 
+            this.gbAdd.Controls.Add(this.bttAdd);
+            this.gbAdd.Controls.Add(this.cbGroups);
+            this.gbAdd.Controls.Add(this.tbFio);
+            this.gbAdd.Controls.Add(this.lblGroups);
+            this.gbAdd.Controls.Add(this.lblFio);
+            this.gbAdd.Location = new System.Drawing.Point(12, 386);
+            this.gbAdd.Name = "gbAdd";
+            this.gbAdd.Size = new System.Drawing.Size(731, 52);
+            this.gbAdd.TabIndex = 2;
+            this.gbAdd.TabStop = false;
+            this.gbAdd.Text = "Добавить";
+            // 
+            // bttAdd
+            // 
+            this.bttAdd.Location = new System.Drawing.Point(423, 19);
+            this.bttAdd.Name = "bttAdd";
+            this.bttAdd.Size = new System.Drawing.Size(75, 23);
+            this.bttAdd.TabIndex = 4;
+            this.bttAdd.Text = "Добавить";
+            this.bttAdd.UseVisualStyleBackColor = true;
+            this.bttAdd.Click += new System.EventHandler(this.bttAdd_Click);
+            // 
+            // cbGroups
+            // 
+            this.cbGroups.FormattingEnabled = true;
+            this.cbGroups.Location = new System.Drawing.Point(296, 20);
+            this.cbGroups.Name = "cbGroups";
+            this.cbGroups.Size = new System.Drawing.Size(121, 21);
+            this.cbGroups.TabIndex = 3;
+            // 
+            // tbFio
+            // 
+            this.tbFio.Location = new System.Drawing.Point(49, 22);
+            this.tbFio.Name = "tbFio";
+            this.tbFio.Size = new System.Drawing.Size(199, 20);
+            this.tbFio.TabIndex = 2;
+            // 
+            // lblGroups
+            // 
+            this.lblGroups.AutoSize = true;
+            this.lblGroups.Location = new System.Drawing.Point(254, 22);
+            this.lblGroups.Name = "lblGroups";
+            this.lblGroups.Size = new System.Drawing.Size(42, 13);
+            this.lblGroups.TabIndex = 1;
+            this.lblGroups.Text = "Группа";
+            // 
+            // lblFio
+            // 
+            this.lblFio.AutoSize = true;
+            this.lblFio.Location = new System.Drawing.Point(7, 23);
+            this.lblFio.Name = "lblFio";
+            this.lblFio.Size = new System.Drawing.Size(34, 13);
+            this.lblFio.TabIndex = 0;
+            this.lblFio.Text = "ФИО";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbAdd);
+            this.Controls.Add(this.gbUsers);
+            this.Controls.Add(this.gbGroups);
+            this.Name = "MainForm";
+            this.Text = "Мониторинг сотрудников";
+            this.gbGroups.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgGroups)).EndInit();
+            this.gbUsers.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgUsers)).EndInit();
+            this.gbAdd.ResumeLayout(false);
+            this.gbAdd.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbGroups;
+        private System.Windows.Forms.DataGridView dgGroups;
+        private System.Windows.Forms.GroupBox gbUsers;
+        private System.Windows.Forms.DataGridView dgUsers;
+        private System.Windows.Forms.GroupBox gbAdd;
+        private System.Windows.Forms.Button bttAdd;
+        private System.Windows.Forms.ComboBox cbGroups;
+        private System.Windows.Forms.TextBox tbFio;
+        private System.Windows.Forms.Label lblGroups;
+        private System.Windows.Forms.Label lblFio;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ TestApp/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class MainForm : Form
+    {
+        DataTable Groups = new DataTable();
+        DataTable Users = new DataTable();
+        DataTable UserGroup = new DataTable();
+
+        string ConnStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\vanka\source\repos\TestApp\TestApp\TestDB.mdf;Integrated Security=True";
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            FillDTs();
+            
+            GetDataForCbGroups();
+            GetDataForDgGroups();
+            GetDataForDgUsers();
+        }
+
+        private void GroupModify()
+        {
+            if (Groups.Columns.Count < 3)
+            {
+                DataColumn qnt = new DataColumn();
+                qnt.ColumnName = "Qnt";
+                Groups.Columns.Add(qnt);
+            }
+
+            foreach (DataRow row in Groups.Rows)
+                row["Qnt"] = GetQnt(row["ID_Group"]);
+        }
+
+        private void UserModify()
+        {
+            if (Users.Columns.Count < 3)
+            {
+                DataColumn list = new DataColumn();
+                list.ColumnName = "Group List";
+                Users.Columns.Add(list);
+            }
+
+            foreach (DataRow row in Users.Rows)
+                row["Group List"] = GetList(row["ID_User"]);
+        }
+
+        private string GetList(object id_user)
+        {
+            string result = "";
+
+            foreach (DataRow row in UserGroup.Rows)
+                if (row["ID_User"].Equals(id_user))
+                    result += (Groups.Select ("ID_Group = " + row["ID_Group"].ToString()) [0]["Description"].ToString() + ";");
+
+            return result;
+        }
+
+        private void GetDataForDgUsers()
+        {
+            dgUsers.DataSource = Users;
+        }
+
+        private void GetDataForDgGroups()
+        {
+            dgGroups.DataSource = Groups;
+        }
+
+        private int GetQnt(object id_group)
+        {
+            int result = 0;
+
+            foreach (DataRow row in UserGroup.Rows)
+                if (row["ID_Group"].Equals(id_group))
+                    result++;
+
+            return result;
+        }
+
+        private void FillDTs()
+        {
+            string query_groups = "SELECT * FROM Groups";
+            string query_users = "SELECT * FROM Users";
+            string query_user_group = "SELECT * FROM UserGroup";
+
+            FillDT(Groups, query_groups);
+            FillDT(Users, query_users);
+            FillDT(UserGroup, query_user_group);
+
+            GroupModify();
+            UserModify();
+        }
+        private void FillDT(DataTable table, string query)
+        {
+            table.Rows.Clear();
+
+            using (SqlConnection connection = new SqlConnection(ConnStr))
+            {
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                adapter.Fill(table);
+                adapter.Dispose();
+            }
+
+        }
+
+        private void GetDataForCbGroups()
+        {
+            cbGroups.DataSource = Groups;
+            cbGroups.DisplayMember = "Description";
+            cbGroups.ValueMember = "Description";
+        }
+
+        private void dgUsers_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                int row_index = dgUsers.HitTest(e.X, e.Y).RowIndex;
+                int user_id = (int)dgUsers.Rows[row_index].Cells[0].Value;
+
+                DeleteUser(user_id);
+
+                FillDTs();
+            }
+        }
+
+        private void ExecuteDel(SqlDataAdapter adapter, SqlConnection connection)
+        {
+
+            //Странный способ, похож на какой-то костыль. Так делал чувак на Youtube
+
+            connection.Open();
+            adapter.DeleteCommand.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void DeleteUser(int user_id)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnStr))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter();
+
+                // PROBABLY, JOIN IS REQUIRED HERE
+                SqlCommand ug_del_cmd = new SqlCommand("DELETE FROM UserGroup WHERE ID_User = @user_id", connection);
+                ug_del_cmd.Parameters.Add("@user_id", SqlDbType.Int);
+                ug_del_cmd.Parameters["@user_id"].Value = user_id;
+                
+                adapter.DeleteCommand = ug_del_cmd;
+                ExecuteDel(adapter, connection);
+
+                SqlCommand u_del_cmd = new SqlCommand("DELETE FROM Users WHERE ID_User = @user_id", connection);
+                u_del_cmd.Parameters.Add("@user_id", SqlDbType.Int);
+                u_del_cmd.Parameters["@user_id"].Value = user_id;
+
+                adapter.DeleteCommand = u_del_cmd;
+                ExecuteDel(adapter, connection);
+
+                adapter.Dispose();
+            }
+        }
+
+        private void ExecuteAdd(SqlDataAdapter adapter, SqlConnection connection)
+        {
+
+            //Странный способ, похож на какой-то костыль. Так делал чувак на Youtube
+
+            connection.Open();
+            adapter.InsertCommand.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void AddUser(string fio, string group)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnStr))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter();
+
+                SqlCommand u_cmd = new SqlCommand("INSERT INTO Users (FIO)" + "VALUES(@FIO)", connection);
+                u_cmd.Parameters.Add("@FIO", SqlDbType.NVarChar);
+                u_cmd.Parameters["@FIO"].Value = fio;
+
+                adapter.InsertCommand = u_cmd;
+                ExecuteAdd(adapter, connection);
+                FillDT(Users, "SELECT * FROM Users");
+
+                int group_id = GetId(group, Groups, "Description", "ID_Group");
+                int user_id = GetId(fio, Users, "FIO", "ID_User");
+                
+                SqlCommand ug_cmd = new SqlCommand("INSERT INTO UserGroup (ID_User, ID_Group)"
+                                                    + "VALUES(@id_user, @id_group)", connection);
+                ug_cmd.Parameters.Add("@id_user", SqlDbType.Int);
+                ug_cmd.Parameters.Add("@id_group", SqlDbType.Int);
+                ug_cmd.Parameters["@id_user"].Value = user_id;
+                ug_cmd.Parameters["@id_group"].Value = group_id;
+
+                adapter.InsertCommand = ug_cmd;
+                ExecuteAdd(adapter, connection);
+
+                adapter.Dispose();
+            }
+        }
+
+        private int GetId(string group, DataTable table, string src, string dst)
+        {
+            int result = 0;
+            DataRow[] rows = table.Select();
+
+            for (int i = 0; i < rows.Length; i++)
+                if (rows[i][src].ToString() == group)
+                    result = (int)rows[i][dst];
+
+            return result;
+        }
+
+        private void bttAdd_Click(object sender, EventArgs e)
+        {
+            string fio = tbFio.Text;
+            string group = cbGroups.SelectedValue.ToString();
+
+            AddUser(fio, group);
+
+            FillDTs();
+        }
+    }
+}
